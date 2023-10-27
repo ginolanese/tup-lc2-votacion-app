c3bf9a39bb698d3f124c9176653d4891447a855c
AgusRey04 <reymundez37@gmail.com>
2023-10-27 15:40:02 -0300

    modified:   js/pages/paso.js

--- js/pages/paso.js
+++ js/pages/paso.js
@@ -1,40 +1,42 @@
 
-
 const tipoEleccion = 1;
 const tipoRecuento = 1;
+var periodosSelect; // Declaración de la variable periodosSelect
 fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
-        .then(res=> res.json(res))
-        .then((res) => {           
-                    var selectAnio = document.getElementById("anio");
-                     res.forEach(anio => {
-                        
-                         const  nuevaOption= document.createElement("option");
-                         nuevaOption.textContent = anio;
+    .then((res) => res.json())
+    .then((res) => {
+        var selectAnio = document.getElementById("anio");
+        res.forEach((anio) => {
+            const nuevaOption = document.createElement("option");
+            nuevaOption.textContent = anio;
+            nuevaOption.value = anio;
+            selectAnio.appendChild(nuevaOption);
+        });
 
-                         nuevaOption.innerHTML = `
-                         ${anio}
-                        `;
-                        nuevaOption.value= anio
-                        selectAnio.appendChild(nuevaOption);
-
-                        console.log(nuevaOption.value)
-                     });
-                     onsole.log(nuevaOption.value) //no me toma el valor de nuevaOption
-                         fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + anio) 
-                         .then(res=> res.json(res))
-                         .then((res) => {
-                                     const selectAnio = document.getElementById("cargo");
-                                     res.forEach(cargo => {
-                                         const  nuevaOption= document.createElement("option");
-                                         nuevaOption.textContent = cargo;
-                                         selectAnio.appendChild(nuevaOption);
-                                         nuevaOption.innerHTML = `
-                                         <option value="${cargo}" >${cargo}</option> 
-                                         `;
-                                         console.log()
-                                     });
-                               } )  
-                    
-              } )  
-
-                           
+        // Asigna el elemento select a la variable periodosSelect
+        periodosSelect = selectAnio;
+        periodosSelect.addEventListener("change", function () {
+            console.log(periodosSelect.value)
+            fetch("https://resultados.mininterior.gob.ar/api/menu?año=" +   periodosSelect.value)
+            .then(res=> res.json(res))
+            .then((datosFiltros) => {
+                          const selectAnio = document.getElementById("cargo");
+                          datosFiltros.forEach(eleccion => {
+                            if (eleccion.IdEleccion == tipoEleccion){
+                                alert("se encontro");
+                                console.log(eleccion.IdEleccion)
+                                
+                            }
+                            console.log(eleccion)
+                            //   const  nuevaOption= document.createElement("option");
+                            //   nuevaOption.textContent = eleccion;
+                            //   selectAnio.appendChild(nuevaOption);
+                            //   nuevaOption.innerHTML = `
+                            //   <option value="${eleccion.Cargos}" >${eleccion.Cargos[1].Cargo}</option> 
+                            //   `;
+                            //   console.log(cargo);
+                          });
+                    } )  
+            
+        })
+    });
