fe4886ea6150d504d27d58313c838acbc24f6141
AgusRey04 <reymundez37@gmail.com>
2023-10-27 10:43:20 -0300

    modified:   generales.html 	modified:   js/common.js 	modified:   js/pages/general.js 	modified:   js/pages/paso.js 	modified:   paso.html

--- js/pages/general.js
+++ js/pages/general.js
@@ -1,1 +1,25 @@
+const tipoEleccion = 1;
+const tipoRecuento = 1;
+var periodosSelect;
+fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
+        .then(res=> res.json(res))
+        .then((res) => {
+                    periodosSelect=res;
+                    
+                    const selectAnio = document.getElementById("anio");
+                    periodosSelect.forEach(anio => {
 
+                        const  nuevaOption= document.createElement("option");
+                        nuevaOption.textContent = anio;
+                        selectAnio.appendChild(nuevaOption);
+                        nuevaOption.innerHTML = `
+                        <option value="none-${anio}" >${anio}</option> 
+                        `
+                    });
+
+            
+                } )  
+fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + periodosSelect.value) 
+                        .then(res=> res.json(res))
+                        .then((res) => {console.log(res)} )
+                        .catch((error) => {(console.error(error))} )
