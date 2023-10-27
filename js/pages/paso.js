c5ace1578ce0b236de7b2427bce5c731e07976bb
AgusRey04 <reymundez37@gmail.com>
2023-10-27 12:47:33 -0300

    fetch()

--- js/pages/paso.js
+++ js/pages/paso.js
@@ -2,29 +2,39 @@
 
 const tipoEleccion = 1;
 const tipoRecuento = 1;
-var periodosSelect;
 fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
         .then(res=> res.json(res))
-        .then((res) => {
-                    periodosSelect=res;
-                    
-                    const selectAnio = document.getElementById("anio");
-                    periodosSelect.forEach(anio => {
-
-                        const  nuevaOption= document.createElement("option");
-                        nuevaOption.textContent = anio;
-                        selectAnio.appendChild(nuevaOption);
-                        nuevaOption.innerHTML = `
-                        <option value="none-${anio}" >${anio}</option> 
-                        `
-                    });
-
-            
-                } )  
-fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + periodosSelect.value) 
-                        .then(res=> res.json(res))
-                        .then((res) => {console.log(res)} )
-                        .catch((error) => {(console.error(error))} )
-
-
+        .then((res) => {           
+                    var selectAnio = document.getElementById("anio");
+                     res.forEach(anio => {
                         
+                         const  nuevaOption= document.createElement("option");
+                         nuevaOption.textContent = anio;
+
+                         nuevaOption.innerHTML = `
+                         ${anio}
+                        `;
+                        nuevaOption.value= anio
+                        selectAnio.appendChild(nuevaOption);
+
+                        console.log(nuevaOption.value)
+                     });
+                     onsole.log(nuevaOption.value) //no me toma el valor de nuevaOption
+                         fetch("https://resultados.mininterior.gob.ar/api/menu?año=" + anio) 
+                         .then(res=> res.json(res))
+                         .then((res) => {
+                                     const selectAnio = document.getElementById("cargo");
+                                     res.forEach(cargo => {
+                                         const  nuevaOption= document.createElement("option");
+                                         nuevaOption.textContent = cargo;
+                                         selectAnio.appendChild(nuevaOption);
+                                         nuevaOption.innerHTML = `
+                                         <option value="${cargo}" >${cargo}</option> 
+                                         `;
+                                         console.log()
+                                     });
+                               } )  
+                    
+              } )  
+
+                           
