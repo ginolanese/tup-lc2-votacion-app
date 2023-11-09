216bb1c97abb54690c2274f8c12d070b3d2508ed
N-Rem <nicolasromero.barrios@gmail.com>
2023-11-09 15:06:53 -0300

    Guarda URL en Cosnt
    Se guarda en const para tener mas orden en el codigo.

--- js/pages/paso.js
+++ js/pages/paso.js
@@ -1,8 +1,12 @@
+const periodosURL = "https://resultados.mininterior.gob.ar/api/menu/periodos";
+const cargoURL = "https://resultados.mininterior.gob.ar/api/menu?año=";
+
+
 const tipoEleccion = 1;
 const tipoRecuento = 1;
 var periodosSelect; // Declaración de la variable periodosSelect
 var cargoSelect;
-fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
+fetch(periodosURL)
   .then((res) => res.json())
   .then((res) => {
     console.log(res);
@@ -21,83 +25,77 @@ fetch("https://resultados.mininterior.gob.ar/api/menu/periodos")
     periodosSelect = selectAnio;
     periodosSelect.addEventListener("change", function () {
       console.log(periodosSelect.value);
-      fetch(
-        "https://resultados.mininterior.gob.ar/api/menu?año=" +
-          periodosSelect.value
-      )
-        .then((res) => res.json(res))
-        .then((datosFiltros) => { console.log(datosFiltros)
-           //-------------- Cargo ----------------------------
-           const selectCrgo = document.getElementById("cargo");
-          while (selectCrgo.firstChild) {
-             //elimina todos los elementos
-             selectCrgo.removeChild(selectCrgo.firstChild);
-           }
-           const nuevaOptionCargo = document.createElement("option");
-           nuevaOptionCargo.textContent = "Cargo";
-           nuevaOptionCargo.value = "Cargo";
-          selectCrgo.appendChild(nuevaOptionCargo);
-           datosFiltros.forEach((eleccion) => {
-            console.log("ELECCIONES ")
-            console.log( eleccion)
-             if (eleccion.IdEleccion == tipoEleccion) {
-                eleccion.Cargos.forEach((cargo) => {
-                  console.log("cargo")
-                  console.log(cargo)
-      
-               const nuevaOption = document.createElement("option");
-                 nuevaOption.value = cargo.IdCargo;
-                nuevaOption.innerHTML = `
-                                                                ${cargo.Cargo}
-                                                            `;
-                 console.log(cargo);
-                 selectCrgo.appendChild(nuevaOption);
-               });
-             }
-           });
-            
-           //-------------- Distrito ----------------------------
-           // corregir no filtra bien
- cargoSelect = selectCrgo
- cargoSelect.addEventListener("change", function () {
-  console.log(cargoSelect.value);
-  alert(cargoSelect.value);
-       const selectDistrito = document.getElementById("distrito");
-       while (selectDistrito.firstChild) 
-       {
-           //elimina todos los elementos
-           selectDistrito.removeChild(selectDistrito.firstChild);
-       }
-       const nuevaOptionDistrito = document.createElement("option");
-       nuevaOptionDistrito.textContent = "Distrito";
-       nuevaOptionDistrito.value = "Distrito";
-       selectDistrito.appendChild(nuevaOptionDistrito);
-       datosFiltros.forEach((eleccion) => {
+      //-------------- Cargo ----------------------------
 
-           if (eleccion.IdEleccion == tipoEleccion) {
-            eleccion.Cargos.forEach((cargo) => {
-              console.log (cargoSelect.value );
-              console.log(cargo.value);
-              cargo.Distritos.forEach((distrito) => {
-                console.log("distrito ")
-              console.log( distrito)
-               const nuevaOption = document.createElement("option");
-               nuevaOption.value = distrito.IdDistritos;
-               nuevaOption.innerHTML = `
-                                                       ${distrito.Distrito
-                                                       }
-                                                   `;
-               console.log(cargo);
-               selectDistrito.appendChild(nuevaOption);
-                                                      });
-                                                   
-     });
-   }
- });
- });
+      fetch(cargoURL + periodosSelect.value)
+        .then((res) => res.json(res))
+        .then((datosFiltros) => {
+          console.log(datosFiltros)
+          const selectCrgo = document.getElementById("cargo");
+          while (selectCrgo.firstChild) {
+            //elimina todos los elementos
+            selectCrgo.removeChild(selectCrgo.firstChild);
+          }
+          const nuevaOptionCargo = document.createElement("option");
+          nuevaOptionCargo.textContent = "Cargo";
+          nuevaOptionCargo.value = "Cargo";
+          selectCrgo.appendChild(nuevaOptionCargo);
+          datosFiltros.forEach((eleccion) => {
+            console.log("ELECCIONES ")
+            console.log(eleccion)
+            if (eleccion.IdEleccion == tipoEleccion) {
+              eleccion.Cargos.forEach((cargo) => {
+                console.log("cargo")
+                console.log(cargo)
+
+                const nuevaOption = document.createElement("option");
+                nuevaOption.value = cargo.IdCargo;
+                nuevaOption.innerHTML = `${cargo.Cargo}`;
+                console.log(cargo);
+                selectCrgo.appendChild(nuevaOption);
+              });
+            }
+          });
+
+          //-------------- Distrito ----------------------------
+          // corregir no filtra bien
+          cargoSelect = selectCrgo
+          cargoSelect.addEventListener("change", function () {
+            console.log(cargoSelect.value);
+            alert(cargoSelect.value);
+            const selectDistrito = document.getElementById("distrito");
+            while (selectDistrito.firstChild) {
+              //elimina todos los elementos
+              selectDistrito.removeChild(selectDistrito.firstChild);
+            }
+            const nuevaOptionDistrito = document.createElement("option");
+            nuevaOptionDistrito.textContent = "Distrito";
+            nuevaOptionDistrito.value = "Distrito";
+            selectDistrito.appendChild(nuevaOptionDistrito);
+            datosFiltros.forEach((eleccion) => {
+
+              if (eleccion.IdEleccion == tipoEleccion) {
+                eleccion.Cargos.forEach((cargo) => {
+                  console.log(cargoSelect.value);
+                  console.log(cargo.value);
+                  cargo.Distritos.forEach((distrito) => {
+                    console.log("distrito ")
+                    console.log(distrito)
+                    const nuevaOption = document.createElement("option");
+                    nuevaOption.value = distrito.IdDistritos;
+
+                    nuevaOption.innerHTML = `${distrito.Distrito}`;
+
+                    console.log(cargo);
+                    selectDistrito.appendChild(nuevaOption);
+                  });
+
+                });
+              }
+            });
+          });
         });
-        
+
     });
   });
 
- 
